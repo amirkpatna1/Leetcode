ebd7268d40da3c8e92f354c89479d62ae40ccc3e
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2022-10-19 10:34:21 +0530

    Time: 22 ms (62.67%), Space: 12.5 MB (76.98%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& vs, int k) {
+        unordered_map<string,int> mp;
+        for(string &s:vs)mp[s]++;
+        vector<pair<string,int>> v(mp.begin(),mp.end());
+        sort(v.begin(),v.end(),[&](pair<string,int> &p1,pair<string,int> &p2){
+           if(p1.second==p2.second)return p1.first<p2.first;
+            return p1.second>p2.second;
+        });
+        vector<string> ans;
+        for(int i=0;i<k;i++)ans.push_back(v[i].first);
+        return ans;
+    }
+};
