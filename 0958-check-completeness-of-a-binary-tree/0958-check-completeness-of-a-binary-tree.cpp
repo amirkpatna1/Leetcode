a05a5ea10183449a4c8ecc91d927804de4b55353
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-03-15 20:04:11 +0530

    Time: 7 ms (43.82%), Space: 10.5 MB (29.89%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -11,32 +11,25 @@
  */
 class Solution {
 public:
-    
-    
     bool isCompleteTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
         bool found = false;
         while(!q.empty()) {
             int sz = q.size();
-            bool flag = false;
-            TreeNode *prev ;
             for(int i = 0; i < sz; i += 1) {
                 TreeNode *node = q.front();
                 q.pop();
-                if(node && (found || (!prev && i != 0) )) return false;
                 if(!node) {
-                    flag = true;
+                    found = true;
                 }
                 if(node) {
+                    if(found) return false;
                     q.push(node->left);
                     q.push(node->right);
                 }
-                prev = node;
                 
             }
-            found = flag;
-            
         }
         return true;
     }
