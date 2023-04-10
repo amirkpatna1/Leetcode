e02ede3753c1a38eb0e7601f46945ced29c3d49e
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-04-10 19:00:50 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (12.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c=='(' || c=='{' || c=='[')st.push(c);
+            else if(st.empty() || (c==']' && (st.top()=='(' || st.top()=='{') )  || (c==')' && (st.top()=='[' || st.top()=='{') ) || (c=='}' && (st.top()=='(' || st.top()=='[') ))return false;
+            else
+                st.pop();
+        }
+        return st.empty();
+    }
+};
