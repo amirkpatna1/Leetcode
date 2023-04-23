3a7a91c0ef8eb0e38df870b6fa773be777edf021
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-04-23 09:46:05 +0530

    Time: 493 ms (10.00%), Space: 186 MB (10.00%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> getSubarrayBeauty(vector<int>& v, int k, int x) {
         vector<int> ans;
-        vector<vector<int>> mp(101);
+        vector<int> mp(101);
         int start = 0,end = 0,n = v.size();
         while(end < n) {
-            mp[v[end] + 50].push_back(v[end++]);
+            mp[v[end] + 50]++;
+            end++;
             if(end - start > k) {
-                mp[v[start] + 50].pop_back();
-                start++;
+                mp[v[start++] + 50]--;
             }
             if(end - start == k) {
                 int cnt = 0, val;
                 for(int i = 0; i <= 100; i += 1) {
-                    cnt += mp[i].size();
+                    cnt += mp[i];
                     if(cnt >= x) {
-                        val = mp[i][0];
+                        val = i - 50;
                         break;
                     }
                 }
