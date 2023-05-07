54249ed746c2f82f46ba89e50abad01c63c9f203
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-05-07 11:01:25 +0530

    Time: 226 ms (50.00%), Space: 135 MB (80.00%) - LeetHub

--- 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.java
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.java
@@ -1,14 +1,15 @@
 class Solution {
-    int ans = 0;
-    int dfs(int n,int[] cost,int i) {
+public:
+    int dfs(int n,vector<int> &cost,int i,int &ans) {
         if(i > n) return 0;
-        int leftCost = dfs(n,cost,2 * i);
-        int rightCost = dfs(n,cost,2 * i + 1);
-        ans += Math.abs(leftCost - rightCost);
-        return Math.max(leftCost,rightCost) + cost[i - 1];
+        int leftCost = dfs(n,cost,2 * i,ans);
+        int rightCost = dfs(n,cost,2 * i + 1,ans);
+        ans += abs(leftCost - rightCost);
+        return max(leftCost,rightCost) + cost[i - 1];
     }
-    public int minIncrements(int n, int[] cost) {
-        dfs(n,cost,1);
+    int minIncrements(int n, vector<int>& cost) {
+        int ans = 0;
+        dfs(n,cost,1,ans);
         return ans;
     }
-}
+};
