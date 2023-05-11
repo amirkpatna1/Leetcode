5e99a265a7e85f3bbdb347564d4c4540a43f105b
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-05-11 17:33:16 +0530

    Time: 14 ms (84.21%), Space: 10.6 MB (84.21%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int dp[501][501];
-    int maxCommonSubseq(vector<int> &v1,vector<int>& v2,int i,int j) {
-        if(i == v1.size() || j == v2.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(v1[i] == v2[j]) return dp[i][j] = 1 + maxCommonSubseq(v1,v2,i + 1, j + 1);
-        return dp[i][j] = max(maxCommonSubseq(v1,v2, i + 1,j),maxCommonSubseq(v1,v2, i, j + 1));
-    }
-    
-    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp,-1,sizeof(dp));
-        return maxCommonSubseq(nums1,nums2,0,0);
+    int maxUncrossedLines(vector<int>& v1, vector<int>& v2) {
+        int n = v1.size(),m = v2.size();
+        for(int i = 0; i <= n; i += 1) dp[i][0] = 0;
+        for(int j = 0; j <= m; j += 1) dp[0][j] = 0;
+        for(int i = 1; i <= n; i += 1) {
+            for(int j = 1; j <= m; j += 1) {
+                if(v1[i - 1] == v2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);
+            }
+        }
+        return dp[n][m];
     }
 };
