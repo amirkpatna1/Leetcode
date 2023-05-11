56ee55e5be76f3701707953cc127aa945e850c5f
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-05-11 17:33:32 +0530

    Time: 12 ms (88.36%), Space: 10.6 MB (84.21%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -3,8 +3,6 @@ public:
     int dp[501][501];
     int maxUncrossedLines(vector<int>& v1, vector<int>& v2) {
         int n = v1.size(),m = v2.size();
-        for(int i = 0; i <= n; i += 1) dp[i][0] = 0;
-        for(int j = 0; j <= m; j += 1) dp[0][j] = 0;
         for(int i = 1; i <= n; i += 1) {
             for(int j = 1; j <= m; j += 1) {
                 if(v1[i - 1] == v2[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
