2b1b9f72956c796a5c5021790012cd592a7f21dd
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-05-12 09:35:48 +0530

    Time: 361 ms (86.07%), Space: 115.2 MB (84.05%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,14 +1,25 @@
 typedef long long ll;
 class Solution {
 public:
-    ll dp[100001];
-    ll dfs(vector<vector<int>> &v,int i) {
-        if(i >= v.size()) return 0;
-        if(dp[i] != -1) return dp[i];
-        return dp[i] = max(dfs(v,i + 1),v[i][0] + dfs(v,i + v[i][1] + 1));
-    }
+    // ll dp[200002];
+    // ll dfs(vector<vector<int>> &v,int i) {
+    //     if(i >= v.size()) return 0;
+    //     if(dp[i] != -1) return dp[i];
+    //     return dp[i] = max(dfs(v,i + 1),v[i][0] + dfs(v,i + v[i][1] + 1));
+    // }
+    
+    // dp[i] --> represents maximum points earned from i -> n 
+    
     long long mostPoints(vector<vector<int>>& v) {
-        memset(dp,-1,sizeof(dp));
-        return dfs(v,0);
+        // memset(dp,-1,sizeof(dp));
+        int n = v.size();
+        vector<ll> dp(n + 1);
+        for(int i = n - 1; i >= 0; i -= 1) {
+            dp[i] = max(dp[i + 1],v[i][0] + dp[min(n,i + v[i][1] + 1)]);
+        }
+        // for(int i = 0; i < n; i += 1) {
+        //     cout<<dp[i]<<" ";
+        // }
+        return dp[0];
     }
 };
