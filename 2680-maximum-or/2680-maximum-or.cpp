f066e4aec6c4e11392886831d7ea97e27c32e1be
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2023-05-14 14:39:22 +0530

    Time: 179 ms (88.89%), Space: 88.8 MB (22.22%) - LeetHub

--- 2680-maximum-or/2680-maximum-or.cpp
+++ 2680-maximum-or/2680-maximum-or.cpp
@@ -3,17 +3,17 @@ class Solution {
 public:
     long long maximumOr(vector<int>& v, int k) {
         int n = v.size();
-        vector<ll> ss(n + 1);
+        vector<ll> suffixOr(n + 1);
         for(int i = n - 1; i >= 0; i -= 1) {
-            ss[i] = v[i] | ss[i + 1];
+            suffixOr[i] = v[i] | suffixOr[i + 1];
         }
-        ll ps = 0;
+        ll prefixOr = 0;
         ll valToOr = 1 << k;
         ll ans = 0;
         for(int i = 0; i < n; i += 1) {
             ll val = v[i] * valToOr;
-            ans = max(ans,ps | val | ss[i + 1]);
-            ps |= v[i];
+            ans = max(ans,prefixOr | val | suffixOr[i + 1]);
+            prefixOr |= v[i];
         }
         return ans;
     }
