bea183c592cecdfbc92d5e5ccc729b65d7ecc469
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-02-21 21:12:57 +0530

    Time: 8 ms (53.91%), Space: 9 MB (8.30%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -5,10 +5,9 @@ public:
         int leftBit = log2(left);
         int rightBit = log2(right);
         if(rightBit > leftBit) return 0;
-        int res = 0;
+        int res = left;
         for(int i = leftBit; i >= 0; i -= 1) {
-            if(((1 << i) & left) ^ ((1 << i) & right)) return res;
-            res |= ((1 << i) & left);
+            if(((1 << i) & left) ^ ((1 << i) & right)) return res & (~((1LL << (i + 1)) - 1));
         }
         return res;
     }
