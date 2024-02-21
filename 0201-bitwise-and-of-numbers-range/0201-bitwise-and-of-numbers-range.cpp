002e4788c8e309521334b22c59c2c4de9f97bb0a
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-02-21 21:05:10 +0530

    Time: 7 ms (64.93%), Space: 8.8 MB (26.31%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -3,7 +3,6 @@ public:
     int rangeBitwiseAnd(int left, int right) {
         // my potential ans --> left 
         if(left == 0) return 0;
-        int ans = left;
         int leftBit = log2(left);
         int rightBit = log2(right);
         if(rightBit > leftBit) return 0;
@@ -12,17 +11,6 @@ public:
             if(!(((1 << i) & left) ^ ((1 << i) & right))) res |= ((1 << i) & left);
             else break;
         }
-        return min(res, ans);
-        // 011
-        // 111
-        
-        // 011
-        // 100
-        // 101
-        // 110
-        // 111
-        // 1001
-        // 1100
-        return ans;
+        return res;
     }
 };
