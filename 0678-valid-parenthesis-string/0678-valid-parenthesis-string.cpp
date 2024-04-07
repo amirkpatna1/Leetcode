77f97e942f37569b8debc5dd506cf3e645e57ea0
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-04-07 18:38:33 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (75.32%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool checkValidString(string &s) {
+        int cmax=0,cmin=0;
+        for(char &c:s){
+            if(c=='(')cmax++,cmin++;
+            else if(c==')')cmax--,cmin--;
+            else cmax++,cmin--;
+            if(cmax<0)return 0;
+            cmin=max(cmin,0);
+        }
+        return cmin==0;
+    }
+};
