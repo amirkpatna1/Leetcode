ad3a3d68ad9eeb8b2e80320401c03086aee51c9d
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-04-10 10:13:03 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (67.22%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& t, int k) {
+        int res = 0;
+        for (int i = 0; i < t.size(); ++i)
+            res += min(t[k] - (i > k), t[i]);
+        return res;
+    }
+};
