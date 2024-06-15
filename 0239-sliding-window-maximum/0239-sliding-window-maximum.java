fe36485a6b5fb3d0746e4c324644c80ce4e6bfed
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-06-16 04:44:43 +0530

    Time: 29 ms (82.06%), Space: 64.2 MB (11.97%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -9,7 +9,7 @@ class Solution {
     }
     
     public int[] maxSlidingWindow(int[] v, int k) {
-        // Deque<Integer> dq = new ArrayDeque<>();
+        Deque<Integer> dq = new ArrayDeque<>();
 //         Queue<Pair> pq = new PriorityQueue<>((Pair a, Pair b) -> b.a - a.a);
 //         for(int i = 0; i < k - 1; i += 1) pq.add(new Pair(v[i], i));
         int n = v.length;
@@ -21,22 +21,28 @@ class Solution {
 //             ans[idx++] = pq.peek().a;
 //         }
         
-//         return ans;
-        
-        TreeMap<Integer, Integer> mp = new TreeMap<>();
-        int start = 0, end = 0;
-        while(end < n) {
-            mp.put(v[end], mp.getOrDefault(v[end++], 0) + 1);
-            if(end - start > k) {
-                int key = v[start];
-                int count = mp.get(key);
-                if(count == 1) mp.remove(key);
-                else mp.put(v[start], mp.get(key) - 1);
-                start += 1;
-            }
-            if(end - start == k) ans[idx++] = mp.lastKey();
-            
+        for(int i = 0; i < n; i += 1) {
+            if(!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
+            while(!dq.isEmpty() && v[dq.peekLast()] < v[i]) dq.pollLast();
+            dq.addLast(i);
+            if(i >= k - 1) ans[idx++] = v[dq.peekFirst()];
         }
         return ans;
+        
+//         TreeMap<Integer, Integer> mp = new TreeMap<>();
+//         int start = 0, end = 0;
+//         while(end < n) {
+//             mp.put(v[end], mp.getOrDefault(v[end++], 0) + 1);
+//             if(end - start > k) {
+//                 int key = v[start];
+//                 int count = mp.get(key);
+//                 if(count == 1) mp.remove(key);
+//                 else mp.put(v[start], mp.get(key) - 1);
+//                 start += 1;
+//             }
+//             if(end - start == k) ans[idx++] = mp.lastKey();
+            
+//         }
+//         return ans;
     }
 }
