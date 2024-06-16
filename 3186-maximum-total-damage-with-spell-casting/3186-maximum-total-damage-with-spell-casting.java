48548d1f375dfe214e83382f14920a06463a6441
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-06-16 11:20:43 +0530

    Time: 220 ms (100.00%), Space: 66.4 MB (100.00%) - LeetHub

---
+++ 3186-maximum-total-damage-with-spell-casting/3186-maximum-total-damage-with-spell-casting.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long maximumTotalDamage(int[] p) {
+        Map<Integer, Integer> keyToFreq = new HashMap<>();
+        Map<Integer, Integer> keyToIndex = new HashMap<>();
+        for(int i = 0; i < p.length; i += 1) {
+            keyToFreq.put(p[i], keyToFreq.getOrDefault(p[i], 0) + 1);
+        }
+        int n = keyToFreq.size();
+        int[] v = new int[n];
+        int idx = 0;
+        for(Integer key : keyToFreq.keySet()) {
+            v[idx++] = key;
+        }
+        Arrays.sort(v);
+        
+        for(int i = 0; i < n; i += 1) {
+            keyToIndex.put(v[i], i);
+        }
+        
+        // dp[i] --> max damage after i th index
+        long[] dp = new long[n];
+        
+        for(int i = 0; i < n; i += 1) {
+            long fp = (i - 1 >= 0 && v[i] - v[i - 1] > 2) ? dp[i - 1] : 0;
+            long sp = (i - 2 >= 0 && v[i] - v[i - 2] > 2) ? dp[i - 2] : 0;
+            long temp = 0;
+            for(int j = Math.max(i - 6, 0); j < i; j += 1) {
+                if(v[i] - v[j] > 2)
+                    temp = Math.max(temp, dp[j]);
+            }
+            dp[i] = (long)v[i] * (long)keyToFreq.get(v[i]) + temp;
+            
+        }
+
+        // [7,1,6,6] --> 1, 6, 7
+        // [5,9,2,10,2,7,10,9,3,8] --> 2, 3, 5, 7, 8, 9, 10
+        // 4, 3, 5, 
+        
+        return Arrays.stream(dp).max().orElseThrow();
+    }
+}
