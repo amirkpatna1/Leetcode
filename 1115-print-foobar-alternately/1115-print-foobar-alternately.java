6e607686c5eecd1271ce26571f99d11897b3ebab
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-06-17 17:23:40 +0530

    Time: 27 ms (11.85%), Space: 43.7 MB (14.30%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,39 +1,48 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 class FooBar {
     private int n;
-    private boolean isFooTurn = true;
+    private volatile boolean isFooTurn = true;
+    private Lock lock = new ReentrantLock();
+    private Condition condition = lock.newCondition();
+
     public FooBar(int n) {
         this.n = n;
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
-        
-        synchronized(this) {
+        lock.lock();
+        try {
             for (int i = 0; i < n; i++) {
-            
-                while(!isFooTurn) {
-                    wait();
+                while (!isFooTurn) {
+                    condition.await();
                 }
-
                 // printFoo.run() outputs "foo". Do not change or remove this line.
                 printFoo.run();
                 isFooTurn = false;
-                notify();
+                condition.signal();
             }
+        } finally {
+            lock.unlock();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
-        
-        synchronized(this) {
+        lock.lock();
+        try {
             for (int i = 0; i < n; i++) {
-                while(isFooTurn) {
-                    wait();
+                while (isFooTurn) {
+                    condition.await();
                 }
                 // printBar.run() outputs "bar". Do not change or remove this line.
                 printBar.run();
                 isFooTurn = true;
-                notify();
+                condition.signal();
             }
+        } finally {
+            lock.unlock();
         }
     }
 }
