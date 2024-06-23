0285e150fce1bef450c922d595cf032e5b34d7bc
AMIR KHAN <74737871+amirkpatna1@users.noreply.github.com>
2024-06-23 14:01:47 +0530

    Time: 19 ms (27.62%), Space: 18.2 MB (21.11%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        queue<pair<int, vector<int>>> q;
+        queue<vector<int>> q;
         int n = graph.size();
-        q.push({0, {0}});
+        q.push({0});
         vector<vector<int>> ans;
         while(!q.empty()) {
-            auto p = q.front();
+            vector<int> res = q.front();
             q.pop();
-            int curr = p.first;
-            vector<int> res = p.second;
+            int curr = res.back();
             if(curr == n - 1) ans.push_back(res);
             for(int x : graph[curr]) {
                 res.push_back(x);
-                q.push({x, res});
+                q.push({res});
                 res.pop_back();
             }
         }
